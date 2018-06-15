fa49c9e721b5fac7c373ba6320940f17b0124d87
Brandon <brandy@scarecrowarts.com>
2018-06-14 21:46:28 -0400

    hey rot try working

--- Sources/arm/CorgiTrait.hx
+++ Sources/arm/CorgiTrait.hx
@@ -12,9 +12,19 @@ class CorgiTrait extends iron.Trait{
 
     function update(){
         if(Input.getKeyboard().down("space")){}else{
-            if(Scene.active.getMesh("Corgi").transform.loc.z > 0){
+            if(Scene.active.getMesh("Corgi").transform.loc.z > 0.1){
                 Scene.active.getMesh("Corgi").transform.translate(0,0,-0.2);
             }
+            if(Scene.active.getMesh("Corgi").transform.loc.z < 0){
+                Scene.active.getMesh("Corgi").transform.translate(0,0,0.1);
+            }
+        }
+        if(Input.getKeyboard().down("left")){
+            Scene.active.getMesh("Corgi").transform.translate(0,-0.1,0);
+            //Scene.active.getMesh("Corgi").transform.rotate();
+        }
+        if(Input.getKeyboard().down("right")){
+            Scene.active.getMesh("Corgi").transform.translate(0,0.1,0);
         }
     }
 }
